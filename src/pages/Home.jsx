5c7f471c15ed0a320ffe651c31f2b9e69b46cbb2
Alireza <aliareya2003@gmail.com>
2025-03-04 16:54:14 +0430

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,16 @@ import PatientsComment from "./homecomponents/PatientsComment";
 import AppointmentForm from "../forms/AppointmentForm";
 import HomeRewards from "./homecomponents/HomeRewards";
 
-
-
 function Home() {
   return (
     <div className=" z-0 w-full bg-white flex flex-col items-center pt-32">
-      <AppointmentForm/>
-      <HowWeWorks/>
-      <MedialServices/>
-      <GreateWork/>
-      <PatientsComment/>
-      <HomeRewards/>
+      <AppointmentForm />
+      <HowWeWorks />
+      <MedialServices />
+      <GreateWork />
+      <PatientsComment />
+      <HomeRewards />
     </div>
-  )
+  );
 }
 export default Home;
--- src/pages/homecomponents/HomeRewards.jsx
+++ src/pages/homecomponents/HomeRewards.jsx
@@ -1,5 +1,5 @@
-import { Icon } from '@iconify-icon/react';
-import { useState, useEffect } from 'react';
+import { Icon } from "@iconify-icon/react";
+import { useState, useEffect } from "react";
 
 function HomeRewards() {
   const [currentNumber, setCurrentNumber] = useState(1);
@@ -13,20 +13,26 @@ function HomeRewards() {
     const handleScroll = () => {
       setY(window.scrollY);
     };
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  const increaseValue = (currentValue, setValue, target, step, intervalTime) => {
+  const increaseValue = (
+    currentValue,
+    setValue,
+    target,
+    step,
+    intervalTime
+  ) => {
     if (currentValue < target) {
       const interval = setInterval(() => {
         setValue((prevValue) => {
           if (prevValue < target) {
             return prevValue + step;
           } else {
-            clearInterval(interval); 
+            clearInterval(interval);
             return prevValue;
           }
         });
@@ -37,10 +43,10 @@ function HomeRewards() {
   useEffect(() => {
     if (y >= 2800) {
       if (satisfiedClients === 0) {
-        increaseValue(satisfiedClients, setSatisfiedClients, 500, 2, 100); 
+        increaseValue(satisfiedClients, setSatisfiedClients, 500, 2, 100);
       }
       if (currentNumber === 1) {
-        increaseValue(currentNumber, setCurrentNumber, 3020, 10, 50); 
+        increaseValue(currentNumber, setCurrentNumber, 3020, 10, 50);
       }
       if (awardWinning === 25) {
         increaseValue(awardWinning, setAwardWinning, 580, 2, 100);
@@ -55,13 +61,29 @@ function HomeRewards() {
       setClientSupport(24);
       setCurrentNumber(1);
     }
-  }, [y, satisfiedClients, awardWinning, clientSupport, currentNumber]); 
+  }, [y, satisfiedClients, awardWinning, clientSupport, currentNumber]);
 
   const rewards = [
-    { Icon: 'healthicons:doctor-male', number: `${satisfiedClients}+`, title: 'Qualified Doctors' },
-    { Icon: 'dashicons:businesswoman', number: `${currentNumber}+`, title: 'Satisfied Clients' },
-    { Icon: 'line-md:star-pulsating-loop', number: `${awardWinning}+`, title: 'Award Winning' },
-    { Icon: 'streamline:customer-support-1-solid', number: `${clientSupport}/7`, title: 'Client Support' },
+    {
+      Icon: "healthicons:doctor-male",
+      number: `${satisfiedClients}+`,
+      title: "Qualified Doctors",
+    },
+    {
+      Icon: "dashicons:businesswoman",
+      number: `${currentNumber}+`,
+      title: "Satisfied Clients",
+    },
+    {
+      Icon: "line-md:star-pulsating-loop",
+      number: `${awardWinning}+`,
+      title: "Award Winning",
+    },
+    {
+      Icon: "streamline:customer-support-1-solid",
+      number: `${clientSupport}/7`,
+      title: "Client Support",
+    },
   ];
 
   return (
@@ -69,10 +91,22 @@ function HomeRewards() {
       <div className="w-[82%] h-72 bg-[#045e74] rounded-xl px-4 flex gap-3 justify-between items-center">
         {rewards.map((reward, index) => {
           return (
-            <div key={index} className="w-[20%] h-64 flex flex-col justify-center items-center ">
-              <Icon icon={reward.Icon} width={90} height={90} style={{ color: '#fff' }} />
-              <h1 className='text-center text-white text-6xl -mt-3 font-[Katibeh]'>{reward.number}</h1>
-              <p className='text-center text-white text-4xl font-[Katibeh]'>{reward.title}</p>
+            <div
+              key={index}
+              className="w-[20%] h-64 flex flex-col justify-center items-center "
+            >
+              <Icon
+                icon={reward.Icon}
+                width={90}
+                height={90}
+                style={{ color: "#fff" }}
+              />
+              <h1 className="text-center text-white text-6xl -mt-3 font-[Katibeh]">
+                {reward.number}
+              </h1>
+              <p className="text-center text-white text-4xl font-[Katibeh]">
+                {reward.title}
+              </p>
             </div>
           );
         })}
